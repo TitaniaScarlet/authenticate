d0090aaf660408da9638bfc611a8941b99ab57a3
TitaniaScarlet <olunchikkk@mail.ru>
2018-09-25 18:00:50 +0200

    Changed

--- setupusers.php
+++ setupusers.php
@@ -6,21 +6,22 @@ if($conn->connect_error) die($conn->connect_error);
 <pre><form action="setupusers.php" method = "post">
     Имя     <input type = "text" name = "forename">
     Фамилия <input type = "text" name = "surname">
-    Логин   <input type = "text" name="username" maxlength="15">
+    Логин   <input type = "text" name="username" >
     Пароль  <input type="password" name="password">
 <input type="submit" value="Зарегистрироваться">
 </form> </pre>
 <?php
-if(isset($_POST['forename']) && isset($_POST['surname']) && isset($_POST['username']) && isset($_POST['password'])) {
-    $username = mysql_entities_fix_string($conn, $_POST['username']);
-    $password = mysql_entities_fix_string($conn, $_POST['password']);
-    $forename = mysql_entities_fix_string($conn, $_POST['forename']);
-    $surname = mysql_entities_fix_string($conn, $_POST['surname']);
-    $salt1 = "gm&h*";
-    $salt2 = "pg!@";
-    $token_ins = hash('ripemd128', "$salt1$password$salt2");
-    $query = "INSERT INTO users VALUES" . "('$forename', '$surname', '$username', '$token_ins')";
-    $result = $conn->query($query);
+if(isset($_POST['forename']) && isset($_POST['surname']) && preg_match('/^[A-z0-9-_]{3,20}$/i', $_POST['username'])
+    && preg_match('/^[A-z0-9]{8,20}$/i', $_POST['password'])) {
+    $stmt = $conn->prepare('INSERT INTO users VALUES(?,?,?,?)');
+    $stmt->bind_param('ssss',$forename,$surname, $username, $hash);
+    $username = $_POST['username'];
+    $forename = $_POST['forename'];
+    $surname = $_POST['surname'];
+    $password = $_POST['password'];
+    $hash = password_hash($password, PASSWORD_DEFAULT, ['cost' => 14]);
+    $stmt->execute();
+    $result = $stmt->affected_rows;
     if (!$result) echo "Пользователь с таким логином уже существует<br>";
     elseif ($result)
     {
@@ -32,18 +33,7 @@ if(isset($_POST['forename']) && isset($_POST['surname']) && isset($_POST['userna
     else {
         die("Пожалуйста, заполните все поля для регистрации");
     }
+    $result->close();
     $conn->close();
-    function mysql_entities_fix_string($conn, $string)
-    {
-        return htmlentities(mysql_fix_string($conn, $string));
-    }
-
-    function mysql_fix_string($conn, $string)
-    {
-        if (get_magic_quotes_gpc())
-            $string = stripslashes($string);
-        $string = $conn->real_escape_string($string);
-        return $string;
-    }
-?>
+    ?>
 
